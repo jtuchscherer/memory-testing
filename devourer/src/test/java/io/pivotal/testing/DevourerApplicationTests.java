cda2c2a0ef82b57d644b0e908ce83593ff0e7281
Johannes Tuchscherer <jtuchscherer@gmail.com>
2017-11-23 23:47:07 +0100

    Updating Spring Boot
    Adding ActuatorAdding NMT metrics

--- devourer/src/test/java/io/pivotal/testing/DevourerApplicationTests.java
+++ devourer/src/test/java/io/pivotal/testing/DevourerApplicationTests.java
@@ -2,13 +2,13 @@ package io.pivotal.testing;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DevourerApplication.class)
-@WebAppConfiguration
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class DevourerApplicationTests {
 
 	@Test
---
+++ narcoleptic/src/main/java/io/pivotal/testing/nmt/NMTNotEnabledException.java
@@ -0,0 +1,4 @@
+package io.pivotal.testing.nmt;
+
+public class NMTNotEnabledException extends Exception {
+}
